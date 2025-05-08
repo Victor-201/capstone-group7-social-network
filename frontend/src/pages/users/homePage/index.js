482307c222691c81a80285bc0efc7866427ef471
Trung <trungbinh2003@gmail.com>
2025-05-08 19:20:23 +0700

    update homepage

--- frontend/src/pages/users/homePage/index.js
+++ frontend/src/pages/users/homePage/index.js
@@ -4,7 +4,6 @@ import { FaUsers, FaSpinner, FaExclamationTriangle } from 'react-icons/fa';
 import CreatePost from '../../../components/createPost';
 import Post from '../../../components/postCard';
 import AddFriendCard, { AddFriendSuggestion } from '../../../components/addFriendCard';
-import { renderReelsSection } from './modals/reels';
 import './style.scss';
 
 const HomePage = () => {
@@ -37,28 +36,29 @@ const HomePage = () => {
   };
 
   return (
-    <div className="home-page">
-      {/* Create Post Section */}
-      <div className="create-post-section">
-        <CreatePost />
-      </div>
-      {/* Posts Feed Section */}
-      <div className="posts-feed">
-        {loading ? (
-          <div className="loading-spinner">
-            <FaSpinner className="spinner" />
-            <span>Loading posts...</span>
-          </div>
-        ) : posts.length === 0 ? (
-          <div className="no-posts">
-            <FaExclamationTriangle />
-            <span>No posts to show</span>
-          </div>
-        ) : (
-          renderPostsWithSuggestions()
-        )}
-      </div>
-
+    <div className="container">
+      <article className="home-page">
+        {/* Create Post Section */}
+        <div className="create-post-section">
+          <CreatePost />
+        </div>
+        {/* Posts Feed Section */}
+        <div className="posts-feed">
+          {loading ? (
+            <div className="loading-spinner">
+              <FaSpinner className="spinner" />
+              <span>Loading posts...</span>
+            </div>
+          ) : posts.length === 0 ? (
+            <div className="no-posts">
+              <FaExclamationTriangle />
+              <span>No posts to show</span>
+            </div>
+          ) : (
+            renderPostsWithSuggestions()
+          )}
+        </div>
+      </article>
     </div>
   );
 };
